b6d7912d350998d4ce2d16064203fb842cc8194c
paulo.soares <paulo.soares@ifsc.edu.br>
2022-05-12 17:39:30 -0300

    aula-01

---
+++ src/TesteLeitura.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+//Código omitido
+
+public class TesteLeitura {
+
+      public static void main(String[] args) throws IOException {
+
+              //Fluxo de Entrada com Arquivo
+              InputStream fis = new FileInputStream("lorem.txt");
+              Reader isr = new InputStreamReader(fis);
+              BufferedReader br = new BufferedReader(isr);
+
+              String linha = br.readLine();
+
+              while(linha != null) {
+                      System.out.println(linha);
+                      linha = br.readLine();
+              }
+
+              System.out.println(linha);
+
+              br.close();
+      }
+}
